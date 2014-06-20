c029090e51c77789544f42b3cfd2d5791a444357
Vidhu <xx.vidhuxx@gmail.com>
2014-06-20 16:22:58 -0400

    Added Equivalence diagrams

--- JSMathD.js
+++ JSMathD.js
@@ -12,7 +12,7 @@ JSMathD.relation = function(data, options) {
     var relation = data['rel'];
     var fontSize = (options['fontSize'] == null ? 15 : options['fontSize']);
 
-    var r = Raphael(options['holder']);
+    var r = Raphael(options['holder'], options['width'], options['height']);
 
     //pointers
     var exRad = fontSize + (fontSize / 2);
@@ -67,4 +67,83 @@ JSMathD.relation = function(data, options) {
 
 };
 
+JSMathD.equivalence = function(data, options) {
+    var r = Raphael(options['holder'], options['width'], options['height']);
+    var textAttr = {
+        "font-size": 12, 
+        "font-family": "monospace",
+        "text-anchor": "start"
+    };
 
+    for (var key in data) {
+        var maxCharacters = JSMathD.getMaxCharactersInSets(data[key])*10;
+        var numClasses = JSMathD.getNumClasses(data[key]);
+        
+        //Draw Container
+        var labelCont = r.rect(10, 5, key.length*8, 15).attr("fill", "#99DBFF");
+        var labelTxt = r.text(12, 12, key).attr(textAttr);
+        var outter = r.rect(10, 20, 10+(maxCharacters)+10, (numClasses+1)*36); //+1 to allow non classes
+        
+        //Draw equivalence classes
+        var nonClassElements = "";
+        for (var i = 0; i < Object.size(data[key]); i++) {
+            var start_x = 15;
+            var start_y = 25;
+            
+            if (data[key][i] instanceof Array) {
+                
+                //Add elements from set
+                var text = "";
+                for(var j in data[key][i]){
+                    text += data[key][i][j] + ", ";
+                }
+                text = text.substring(0, text.length-2);
+                var class_text = r.text(start_x+5, start_y + 13 + (i * 35), text).attr(textAttr);
+                
+                //Draw inner rect
+                var inner = r.rect(start_x, start_y + (i * 35), (maxCharacters)+10, 30);
+            }else{
+               nonClassElements +=  data[key][i] + ", ";
+            }
+        }
+        
+        //Add non class elements
+        nonClassElements = nonClassElements.substring(0, nonClassElements.length-2);
+        var class_text = r.text(20, 25 + 13 + (numClasses * 35), nonClassElements).attr(textAttr);
+    }
+
+}
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key))
+            size++;
+    }
+    return size;
+};
+
+JSMathD.getNumClasses = function(set){
+    var sum = 0;
+    for (var i = 0; i < Object.size(set); i++) {
+        if (set[i] instanceof Array) {
+            sum++
+        }
+    }
+    return sum;
+}
+
+JSMathD.getMaxCharactersInSets = function(sets){
+    var max = 0;
+    for(var set in sets){
+        var sum = 0;
+        for(var element in sets[set]){
+            sum += sets[set][element].toString().length;
+            sum++;
+        }
+        if(max < sum){
+            max = sum;
+        }
+    }
+    return max;
+}
